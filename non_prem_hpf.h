02716de3e8cda0cce871fb4da12ad2f5d196e063
Fatema <FatemaKhalid96@gmail.com>
2017-10-27 14:46:59 +0200

    Non prempitive HPF algorithm

--- non_prem_hpf.h
+++ non_prem_hpf.h
@@ -1,3 +1,29 @@
 #pragma once
 
 #include "for_includes.h"
+
+void NONPRE_HPF(priority_queue<processData> &ready_queue){			
+        // receiving priority queue
+	while(!ready_queue.empty()){
+
+		processData processRunning;
+		processRunning.process_id=fork();
+		processRunning=ready_queue.top();
+		ready_queue.pop();
+		if(processRunning.process_id==-1)
+		{
+
+			perror("fork");
+		}
+		else if (processRunning.process_id==0)
+		{
+			processRunning.waitingTime=processRunning.arrivaltime-getClk();
+			stringstream ss;
+        	ss<<processRunning.remainingtime;
+             execl("./process.out","sch.out","-r", "-t", "-l",ss.str().c_str(), (char *) 0);
+            printf("process Running waiting Time %d \n", processRunning.waitingtime);
+		}
+
+
+	}
+}
